d724571e108d28b43170208575b4b388bcf7c69a
Joe Burgess <joe@flatironschool.com>
2014-11-24 07:15:41 -0500

    Adding solution

--- swift-wwdc-badges/AppDelegate.swift
+++ swift-wwdc-badges/AppDelegate.swift
@@ -17,6 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
 
+        let conferenceSpeakers : [String] = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Wozniak"]
+        for speaker in conferenceSpeakers
+        {
+            println("Hello, my name is \(speaker)")
+        }
 
         return true
     }
