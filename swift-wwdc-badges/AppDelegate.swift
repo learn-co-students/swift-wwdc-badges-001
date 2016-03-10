62805977ec2c98d88bd910b99e158deafa683fed
JeffChiu <jchiu@JeffChius-MacBook-Pro.local>
2016-03-09 18:18:53 -0800

    swift 2.0

--- swift-wwdc-badges/AppDelegate.swift
+++ swift-wwdc-badges/AppDelegate.swift
@@ -6,23 +6,28 @@
 //  Copyright (c) 2014 Flatiron School. All rights reserved.
 //
 
+
+
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-
-
+        
         // Insert code here
 
-
+        let conferenceSpeakers: [String] = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Wozniak"]
+        
+        for speaker in conferenceSpeakers
+        {
+            print("Hello, my name is \(speaker)")
+        }
+        
         return true
     }
-
-
+    
 }
-
